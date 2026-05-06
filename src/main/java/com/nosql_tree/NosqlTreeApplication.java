d31e4dcda938132f0c27d3c15bd6ddc2ca6e06be
Chiwii8 <alejandrosanrod@outlook.com>
2026-05-06 19:16:37 +0200

    Created Default Project

---
+++ src/main/java/com/nosql_tree/NosqlTreeApplication.java
@@ -0,0 +1,13 @@
+package com.nosql_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nosql_tree/NosqlTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosql_tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
